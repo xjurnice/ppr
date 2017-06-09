1bc5210840a6709b9673a1532fb4420244146071
Tomáš Jurníček <tomas.jurnicek@cleverlance.com>
2017-06-09 14:11:31 +0200

    uprava

--- PP/src/pp/brute.java
+++ PP/src/pp/brute.java
@@ -5,7 +5,7 @@
  */
 package pp;
 
-import static java.nio.file.Files.size;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 
 /**
@@ -13,38 +13,32 @@ import java.util.ArrayList;
  * @author tjurnicek
  */
 public class brute {
-    static ArrayList<String> results = new ArrayList<String>();
-    final static String heslo = "accc"; //nase tajne heslo, ktere chceme prolomit
-    static String [] pole;
 
-    char[] canUse = {'a', 'b', 'c', 'd'};
-    int maxlen = 4;
+    static ArrayList<String> results = new ArrayList<String>();
+    final static String heslo = "srnka"; //nase tajne heslo, ktere chceme prolomit
+
+    char[] znaky = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    int maxlen = 5;
 
     public static void main(String[] args) {
-       brute b = new brute();
+        brute b = new brute();
         System.out.println("Nase tajne heslo je " + heslo + " s hashem " + MD5(heslo));
-        
-        if ((heslo) == (heslo)){
-        System.out.println("Yes");
+
+        for (int counter = 0; counter < results.size(); counter++) {
+            //System.out.println(counter + ". " + results.get(counter));
+            String md5String = MD5(results.get(counter));
+            String md5Heslo = MD5(heslo);
+            if (md5String.equals(md5Heslo)) {
+                System.out.println("Naslo se na " + counter + " s hashem " + md5String);
+                return;
+            }
         }
-        
-      for (int counter = 0; counter < results.size(); counter++) { 		      
-          System.out.println(counter + ". " + results.get(counter));
-          String md5String = MD5(results.get(counter));
-          String m5Heslo = MD5(heslo);
-          if (md5String.equals(m5Heslo)){
-          System.out.println("Naslo se na " +  counter + " s hashem " + md5String);
-          
-          }
-      }   
-        	
-		
 
     }
 
     static public String MD5(String md5) {
         try {
-            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            MessageDigest md = java.security.MessageDigest.getInstance("MD5");
             byte[] array = md.digest(md5.getBytes());
             StringBuffer sb = new StringBuffer();
             for (int i = 0; i < array.length; ++i) {
@@ -58,25 +52,23 @@ public class brute {
 
     public brute() {
         int k = 0;
-        while (k < canUse.length) {
-           nextString(new Character(canUse[k]).toString());
+        while (k < znaky.length) {
+            nextString(new Character(znaky[k]).toString());
             k++;
         }
     }
 
-
     private void nextString(String s) {
         String zahesHeslo = MD5(heslo);
-        
-        int i = 0;
-       //System.out.println(s);
-        
-        while (i < canUse.length) {
 
-          // System.out.println(s + new Character(canUse[i]).toString());
-            results.add(s + new Character(canUse[i]).toString());
-            if (new String(s + new Character(canUse[i]).toString()).length() <= maxlen - 1) {
-                nextString(s + new Character(canUse[i]).toString());
+        int i = 0;
+        //System.out.println(s);
+
+        while (i < znaky.length) {
+
+            results.add(s + new Character(znaky[i]).toString()); //vkladame do Arraylistu
+            if (new String(s + new Character(znaky[i]).toString()).length() <= maxlen - 1) {
+                nextString(s + new Character(znaky[i]).toString());
             }
             i++;
         }
